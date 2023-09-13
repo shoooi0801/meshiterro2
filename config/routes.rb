ef4a1453607feabc7e0624d9f4847b1a2c5000b3
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-09-13 10:01:19 +0000

    app19章から

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get "/homes/about" => "homes#about", as: "about"
   resources :users, only: [:show, :edit, :update]
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resources :post_comments, only: [:create, :destroy]
+  end
 end
