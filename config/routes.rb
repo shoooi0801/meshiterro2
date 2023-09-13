52223aae91709934bb300f46537831d79cc3e65a
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-09-13 09:05:49 +0000

    app16章まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   root to: 'homes#top'
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get "/homes/about" => "homes#about", as: "about"
+  resources :users, only: [:show, :edit, :update]
 end
